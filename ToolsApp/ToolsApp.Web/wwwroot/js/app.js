01f92a7b184b81d5f5ce5bb7833a38292dd2aec4
Eric Greene <eric@t4d.io>
2022-01-21 12:19:34 -0500

    js to car tool

--- ToolsApp/ToolsApp.Web/wwwroot/js/app.js
+++ ToolsApp/ToolsApp.Web/wwwroot/js/app.js
@@ -5,3 +5,20 @@ window.toolsAppDemo = {};
 window.toolsAppDemo.setFocus = function (control) {
   control.focus();
 };
+
+
+
+
+window.refreshCarsCaller = (dotnetHelper) => {
+  window.dotnetHelper = dotnetHelper;
+  console.log(window.dotnetHelper);
+
+  if (window.refreshCarsHandle) {
+    window.clearTimeout(window.refreshCarsHandle);
+  }
+
+  window.refreshCarsHandle = setTimeout(() => {
+    console.log("refreshing cars");
+    dotnetHelper.invokeMethodAsync('ToolsApp', 'RefreshCarsCaller');
+  },  5000);
+}
